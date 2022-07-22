9d2a5d900122a481c142493a88396227375175ac
Sharik <66831646+pycoder213@users.noreply.github.com>
2022-07-22 19:33:44 +0100

    Deez Nutz

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame, os, time, random
+pygame.font.init()
+pygame.init()
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (30, 255, 65)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+
+SC_WIDTH, SC_LENGTH = 700, 450
+
+WIN = pygame.display.set_mode((SC_WIDTH, SC_LENGTH))
+WIN.display.set_caption("Deez")
+
+
+def main():
+    clock = pygame.time.Clock
+
+    running = True   
+
+
+if __name__ == "__main__":
+    main()
